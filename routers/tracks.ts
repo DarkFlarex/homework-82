3c3ce7a56df341e00e667a7e00d4418dd9d7570a
DarkFlarex <darkflarexxx@gmail.com>
2024-09-03 04:11:29 +0600

    Добавлен роут post для создания треков в tracksRouter

---
+++ routers/tracks.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import {TrackMutation} from "../types";
+import mongoose from "mongoose";
+import Track from "../models/Track";
+
+const tracksRouter = express.Router();
+
+tracksRouter.post('/',async (req, res ,next) =>{
+    try{
+        const trackMutation:TrackMutation = {
+            album: req.body.album,
+            nameTrack: req.body.nameTrack,
+            duration: req.body.duration,
+        };
+
+        const track = new Track(trackMutation);
+        await track.save();
+        return res.send(track);
+    } catch (error){
+        if (error instanceof mongoose.Error.ValidationError){
+            return  res.status(400).send(error);
+        }
+        return next(error);
+    }
+});
+
+
+
+export default tracksRouter
