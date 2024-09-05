f1aba8727fd5c406a05daa9f3e2915e655a4211e
DarkFlarex <darkflarexxx@gmail.com>
2024-09-06 03:59:01 +0600

    Добавлен маршрут POST для аутентификации пользователя: проверка пароля и генерация нового токена

--- routers/users.ts
+++ routers/users.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import express from "express";
 import User from "../models/User";
+import TrackHistory from "../models/TrackHistory";
+import Track from "../models/Track";
 
 const usersRouter = express.Router();
 
@@ -24,5 +26,27 @@ usersRouter.post("/", async (req, res, next) => {
     }
 });
 
+usersRouter.post('/sessions', async (req, res, next) => {
+    try{
+        const user = await User.findOne({username: req.body.username});
+
+        if(!user){
+            return res.status(400).send({error: 'Username not found!'});
+        }
+
+        const isMatch = await user.checkPassword(req.body.password);
+
+        if(!isMatch) {
+            return res.status(400).send({error: 'Password is wrong!'});
+        }
+
+        user.generateToken();
+        await user.save();
+
+        return res.send(user);
+    } catch (error) {
+        return next(error);
+    }
+});
 
 export default usersRouter;
