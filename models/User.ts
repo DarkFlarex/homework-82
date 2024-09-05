efa1acf9c669f47baa97d06c9ebfc068aff1161a
DarkFlarex <darkflarexxx@gmail.com>
2024-09-06 03:50:55 +0600

    Добавлена предварительная обработка сохранения для хэширования пароля. Настроен вывод модели без пароля. Добавлен в предыдущем коммите модель TrackHistory с валидацией для пользователей и треков

--- models/User.ts
+++ models/User.ts
@@ -32,6 +32,23 @@ UserSchema.methods.generateToken = function (){
     this.token = randomUUID();
 };
 
+UserSchema.pre('save',async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+
+UserSchema.set('toJSON', {
+    transform: (_doc, ret) => {
+        delete ret.password;
+        return ret;
+    },
+});
+
 const User = mongoose.model<UserFields, UserModel>('User', UserSchema);
 
 export default User;
