1255f5d6f3e9b3c4800a2a461a7dcd70f319d5f8
DarkFlarex <darkflarexxx@gmail.com>
2024-09-06 04:00:45 +0600

    Добавлен маршрут POST /track_history: сохранение информации о прослушанном треке с проверкой токена, пользователя и ID трека

--- routers/users.ts
+++ routers/users.ts
@@ -49,4 +49,49 @@ usersRouter.post('/sessions', async (req, res, next) => {
     }
 });
 
+usersRouter.post('/track_history', async (req, res, next) => {
+    const headerValue = req.get('Authorization');
+    console.log(headerValue);
+    if(!headerValue){
+        return res.status(401).send({error: 'Header "Authorization" not found!'});
+    }
+
+    const [_bearer, token] = headerValue.split(' ');
+
+    if (!token) {
+        return res.status(401).send({error: 'Token not found!'});
+    }
+
+    const user = await User.findOne({token});
+
+    if(!user) {
+        return res.status(401).send({error: 'Wrong Token!'});
+    }
+
+    const trackId = req.body.track;
+
+    if (!trackId) {
+        return res.status(400).send({ error: 'Track ID not found!' });
+    }
+
+    const track = await Track.findById(trackId);
+
+    if (!track) {
+        return res.status(400).send({ error: 'Track not found!' });
+    }
+
+    try {
+        const trackHistory = new TrackHistory({
+            userListened: user._id,
+            trackListened: track._id,
+            datetime: new Date(),
+        });
+
+        await trackHistory.save();
+        return res.send(trackHistory);
+    } catch (error) {
+        return next(error);
+    }
+});
+
 export default usersRouter;
