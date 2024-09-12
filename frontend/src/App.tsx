60ccb3b705dca12fd99097b0d35b6680cf35655a
DarkFlarex <darkflarexxx@gmail.com>
2024-09-13 05:27:02 +0600

    Добавлен роуты в App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import {Container, Typography} from "@mui/material";
 import {Route, Routes} from "react-router-dom";
 import Artists from "./features/artists/Artists";
 import Albums from "./features/albums/Albums";
+import Tracks from "./features/tracks/Tracks";
 
 const App =() => {
 
@@ -13,8 +14,9 @@ const App =() => {
       </header>
       <Container maxWidth="xl" component="main">
         <Routes>
-            <Route path="/albums/:id" element={<Albums />} />
             <Route path="/" element={<Artists />} />
+            <Route path="/tracks/:id" element={<Tracks />} />
+            <Route path="/albums/:id" element={<Albums />} />
             <Route path="*" element={<Typography variant="h1">Not found</Typography>} />
         </Routes>
       </Container>
