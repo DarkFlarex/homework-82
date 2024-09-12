3cdc99ab8f4267c4d6ab77947c1adfe8703e25fa
DarkFlarex <darkflarexxx@gmail.com>
2024-09-13 00:59:27 +0600

    в store добавили редюсер альбом

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {artistsReducer} from "../features/artists/artistsSlice";
+import {AlbumsReducer} from "../features/albums/albumsSlice";
 
 export const store = configureStore({
   reducer: {
     artists:artistsReducer,
+    albums:AlbumsReducer,
   },
 });
 
