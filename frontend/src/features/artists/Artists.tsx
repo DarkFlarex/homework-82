8b8daae6533a5b45187fc9954f46f0e5133469ec
DarkFlarex <darkflarexxx@gmail.com>
2024-09-12 22:26:28 +0600

    Добавлен компонент Artists для отображения списка артистов. Реализована логика загрузки данных с использованием Redux, вывод спиннера во время загрузки и оповещение при отсутствии данных.

---
+++ frontend/src/features/artists/Artists.tsx
@@ -0,0 +1,38 @@
+import React, {useEffect} from 'react';
+import {useAppDispatch, useAppSelector} from "../../app/hooks";
+import {selectArtists, selectArtistsFetching} from "./artistsSlice";
+import {fetchArtists} from "./artistsThunks";
+import {Alert, CircularProgress, Grid} from "@mui/material";
+import ArtistsItem from "./components/ArtistsItem";
+
+const Artists = () => {
+    const dispatch = useAppDispatch();
+    const artists = useAppSelector(selectArtists);
+    const isFetching = useAppSelector(selectArtistsFetching);
+
+    useEffect(() => {
+        dispatch(fetchArtists());
+    }, [dispatch]);
+
+    let content:React.ReactNode = <Alert severity="info" sx={{width: '100%'}}>There are no Artists here!</Alert>;
+    if(isFetching) {
+        content = <CircularProgress/>;
+    } else if (artists.length > 0) {
+        content = artists.map((artist) => (
+            <ArtistsItem
+                key={artist._id}
+                _id={artist._id}
+                name={artist.name}
+                image={artist.image}
+                information={artist.information}
+            />
+        ));
+    }
+    return (
+        <Grid item container spacing={1}>
+            {content}
+        </Grid>
+    );
+};
+
+export default Artists;
