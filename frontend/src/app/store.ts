74b7765c2bc3083a761d633efecc9ceb31dbde1c
DarkFlarex <darkflarexxx@gmail.com>
2024-09-17 12:03:18 +0600

    homework-86: Добавляем в store редюсеры для users

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,15 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import {artistsReducer} from "../features/artists/artistsSlice";
 import {albumsReducer} from "../features/albums/albumsSlice";
 import {tracksReducer} from "../features/tracks/tracksSlice";
+import {usersReducer} from "../features/users/usersSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistStore } from 'redux-persist';
+
+const usersPersistConfig = {
+  key: 'music:users',
+  storage,
+  whitelist: ['user']
+};
+
+const rootReducer = combineReducers({
+  artists: artistsReducer,
+  albums: albumsReducer,
+  tracks: tracksReducer,
+  users: persistReducer(usersPersistConfig, usersReducer),
+});
 
 export const store = configureStore({
-  reducer: {
-    artists:artistsReducer,
-    albums:albumsReducer,
-    tracks:tracksReducer,
+  reducer: rootReducer,
+  middleware:(getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck:{
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE,REGISTER],
+      }
+    });
   },
 });
+export const persistor = persistStore(store);
+
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
