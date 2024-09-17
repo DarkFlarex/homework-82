fadfa3bfd3845663a387a94d58ca409957eb10d0
DarkFlarex <darkflarexxx@gmail.com>
2024-09-17 12:04:47 +0600

    homework-86: Добавляем в app пути

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Artists from "./features/artists/Artists";
 import Albums from "./features/albums/Albums";
 import Tracks from "./features/tracks/Tracks";
 import AppToolbar from "./UI/AppToolbar/AppToolbar";
+import Register from "./features/users/Register";
+import Login from "./features/users/login";
 
 const App =() => {
 
@@ -17,6 +19,8 @@ const App =() => {
             <Route path="/" element={<Artists />} />
             <Route path="/tracks/:id" element={<Tracks />} />
             <Route path="/albums/:id" element={<Albums />} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/login" element={<Login/>} />
             <Route path="*" element={<Typography variant="h1">Not found</Typography>} />
         </Routes>
       </Container>
