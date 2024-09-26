eab1b7a22a95f9a4d26f8a102517c088601e2dbe
DarkFlarex <darkflarexxx@gmail.com>
2024-09-27 03:23:39 +0600

    homework-89: В albumsThunks добавили функцию для добавления альбомов в slice реализованы стейты для создания и обработки ошибок

--- frontend/src/features/albums/albumsSlice.ts
+++ frontend/src/features/albums/albumsSlice.ts
@@ -1,17 +1,21 @@
-import {Album} from "../../types";
+import {Album, GlobalError} from "../../types";
 import {createSlice} from "@reduxjs/toolkit";
-import {fetchAlbumsOneArtist} from "./albumsThunks";
+import {createAlbum, fetchAlbumsOneArtist} from "./albumsThunks";
 
 export interface AlbumsState {
     items: Album[];
     itemsFetching: boolean;
     artistName: string | null;
+    isCreating: boolean;
+    isCreatingError: GlobalError | null;
 }
 
 const initialState: AlbumsState = {
     items: [],
     artistName: null,
     itemsFetching: false,
+    isCreating: false,
+    isCreatingError: null,
 };
 
 export const albumsSlice = createSlice({
@@ -31,12 +35,25 @@ export const albumsSlice = createSlice({
             .addCase(fetchAlbumsOneArtist.rejected, (state) => {
                 state.itemsFetching = false;
             });
+        builder
+            .addCase(createAlbum.pending, (state) => {
+                state.isCreating = true;
+                state.isCreatingError = null;
+            })
+            .addCase(createAlbum.fulfilled, (state) => {
+                state.isCreating = false;
+            })
+            .addCase(createAlbum.rejected, (state, { payload: error }) => {
+                state.isCreating = false;
+                state.isCreatingError = error || null;
+            });
     },
     selectors:{
         selectAlbums:(state) => state.items,
         selectAlbumsFetching:(state) =>state.itemsFetching,
         selectArtistName:(state) => state.artistName,
-
+        selectAlbumCreating:(state) => state.isCreating,
+        selectAlbumCreateError:(state) => state.isCreatingError,
     }
 });
 
@@ -46,4 +63,5 @@ export const {
     selectAlbums,
     selectAlbumsFetching,
     selectArtistName,
+    selectAlbumCreating,
 } = albumsSlice.selectors;
--- frontend/src/features/albums/albumsThunks.ts
+++ frontend/src/features/albums/albumsThunks.ts
@@ -1,11 +1,43 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import {Album} from "../../types";
+import {Album, AlbumMutation, GlobalError} from "../../types";
 import axiosApi from "../../axiosApi";
+import {RootState} from "../../app/store";
+import {isAxiosError} from "axios";
 
 export const fetchAlbumsOneArtist = createAsyncThunk<Album[], string>(
     'albums/fetchAll',
     async (id) => {
         const {data: albums} = await axiosApi.get<Album[]>(`/albums?artist=${id}`);
         return albums;
+    }
+);
+
+export const createAlbum = createAsyncThunk<void, AlbumMutation, { rejectValue: GlobalError; state: RootState }>(
+    'albums/create',
+    async (albumMutation, { getState, rejectWithValue }) => {
+
+        const token = getState().users.user?.token;
+
+        if (!token) {
+            return rejectWithValue({ error: 'User token is missing' });
+        }
+
+        const formData = new FormData();
+        const keys = Object.keys(albumMutation) as (keyof AlbumMutation)[];
+        keys.forEach((key) => {
+            const value = albumMutation[key];
+            if (value !== null) {
+                formData.append(key, value);
+            }
+        });
+
+        try {
+            await axiosApi.post('/albums', formData);
+        } catch (e) {
+            if (isAxiosError(e) && e.response) {
+                return rejectWithValue(e.response.data);
+            }
+            throw e;
+        }
     }
 );
