9522514e21512ef867455e68fb52805f1cb802f2
DarkFlarex <darkflarexxx@gmail.com>
2024-09-26 19:44:47 +0600

    homework-89: Обновили типы данных

--- backend/types.ts
+++ backend/types.ts
@@ -1,30 +1,10 @@
 import {Model} from "mongoose";
 
-export type ArtistMutation = {
-    name:string;
-    image:string | null ;
-    information: string;
-}
-
-export type AlbumMutation = {
-    artist: string;
-    nameAlbum: string;
-    datetime: number;
-    image: string | null;
-};
-
-export type TrackMutation = {
-    album:string;
-    artist:string;
-    nameTrack:string;
-    duration:string;
-    numberTrack:number;
-};
-
 export interface UserFields {
     username: string;
     password: string;
     token:string;
+    role: string;
 }
 
 export interface UserMethods {
