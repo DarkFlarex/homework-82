11dbe3e52021d8b581d640764cf105a02fc123de
DarkFlarex <darkflarexxx@gmail.com>
2024-09-26 19:05:42 +0600

    homework-89: Добавили ProtectedRoute для TrackHistories , для ограничения доступа перенаправления неавторизованных пользователей на страницу входа"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,12 @@ import AppToolbar from "./UI/AppToolbar/AppToolbar";
 import Register from "./features/users/Register";
 import Login from "./features/users/login";
 import TrackHistories from "./features/TrackHistory/TrackHistories";
+import ProtectedRoute from "./UI/ProtectedRoute/ProtectedRoute";
+import {useAppSelector} from "./app/hooks";
+import {selectUser} from "./features/users/usersSlice";
 
 const App =() => {
-
+    const user = useAppSelector(selectUser);
   return (
     <>
       <header>
@@ -22,7 +25,14 @@ const App =() => {
             <Route path="/albums/:id" element={<Albums />} />
             <Route path="/register" element={<Register/>} />
             <Route path="/login" element={<Login/>} />
-            <Route path="/track_history" element={<TrackHistories/>} />
+            <Route
+                path="/track_history"
+                element={
+                    <ProtectedRoute isAllowed={user && user.role === 'admin'}>
+                        <TrackHistories />
+                    </ProtectedRoute>
+            }
+            />
             <Route path="*" element={<Typography variant="h1">Not found</Typography>} />
         </Routes>
       </Container>
