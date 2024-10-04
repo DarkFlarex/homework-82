c3367c59d493b4d44c6a6368c16926f81e572135
DarkFlarex <darkflarexxx@gmail.com>
2024-10-04 13:10:41 +0600

    homework-91: В роут users Добавили в post запрос для регистрации displayName, avatar. Добавлена регистрация и авторизация через Google

--- backend/routers/users.ts
+++ backend/routers/users.ts
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
 import express from "express";
 import User from "../models/User";
+import {OAuth2Client} from "google-auth-library";
+import config from "../config";
+import {imagesUpload} from "../multer";
 
 const usersRouter = express.Router();
 
-usersRouter.post("/", async (req, res, next) => {
+const googleClient = new OAuth2Client(config.google.clientId);
+
+usersRouter.post("/", imagesUpload.single('avatar'), async (req, res, next) => {
     try {
         const user = new User({
             username: req.body.username,
             password: req.body.password,
+            displayName: req.body.displayName,
+            avatar: req.file ? req.file.filename : null,
         });
         user.generateToken();
 
@@ -47,6 +54,49 @@ usersRouter.post('/sessions', async (req, res, next) => {
         user.generateToken();
         await user.save();
 
+        return res.send(user);
+    } catch (error) {
+        return next(error);
+    }
+});
+
+usersRouter.post('/google', async (req, res, next) => {
+    try {
+        const ticket = await googleClient.verifyIdToken({
+            idToken: req.body.credential,
+            audience: config.google.clientId,
+        });
+
+        const payload = ticket.getPayload();
+
+        if (!payload) {
+            return res.status(400).send({error: 'Google Login Error!'});
+        }
+
+        const email = payload.email;
+        const id = payload.sub;
+        const displayName = payload.name;
+        const avatar = payload.picture;
+
+        if (!email) {
+            return res.status(400).send({error: 'Not enough user data to continue!'});
+        }
+
+        let user = await User.findOne({ googleID: id });
+
+        if (!user) {
+            const newPassword = crypto.randomUUID();
+            user = new User({
+                username: email,
+                password: newPassword,
+                googleID: id,
+                displayName,
+                avatar,
+            });
+        }
+
+        user.generateToken();
+        await user.save();
         return res.send(user);
     } catch (error) {
         return next(error);
